b9e7752070b53e3e6a4aebfe5fa4d6df4fb327ac
Yasmin elkoshy <yasmin.elkoshy@gmail.com>
2025-10-09 09:23:25 +0200

    canvas

--- Home/test/3rd week/Triangle.js
+++ Home/test/3rd week/Triangle.js
@@ -33,15 +33,33 @@ async function testTriangleType(driver,sides,expected) {
 
   
     
-    assert.equal(resultText,expected);
+    assert.equal(resultText,expected[0]);
     console.log('✅ Test Passed: Correct triangle type identified');
-    let width = await driver.findElement({id:'triangle-canvas'}).getAttribute('width')
-    assert.notEqual(width,0)
-    let height = await driver.findElement({id:'triangle-canvas'}).getAttribute('height')
-    assert.notEqual(height,0)
+    const script = `
+        const canvas = document.querySelector("#triangle-canvas");
+        if (!canvas) {
+            return null; // Return null if canvas is not found
+        }
+        const ctx = canvas.getContext('2d');
+        const imageData = ctx.getImageData(0, 0, 1, 1);
+        return Array.from(imageData.data); // Returns [R, G, B, A]
+        
+    `;
+    const pixelData = await driver.executeScript(script);
+    
+    if(expected[1]){
+        assert.notEqual(pixelData[3],0);
+    }else{
+        assert.equal(pixelData[3],0);
+    }
+    console.log("✅ Successfully retrieved pixel data.");
+    console.log(pixelData);
+ 
+    
 
   }catch (error) {
         console.error('Test encountered an error:', error);
+        
     }
 }
 
@@ -51,12 +69,14 @@ async function runTest() {
         driver = await new Builder().forBrowser('chrome').build();
         await driver.get('https://testpages.eviltester.com/styled/apps/triangle/triangle001.html')  
         const cases = [
-            { sides: [3, 3, 3], expected: 'Equilateral' },
-            { sides: [5, 5, 8], expected: 'Isosceles' },
-            { sides: [5, 4, 3], expected: 'Scalene' },
-            { sides: [1, 2, 3], expected: 'Error: Not a Triangle' },
-            { sides: [4, 'e', 6], expected: 'Error: Side 2 is not a Number' },
-            { sides: ['%', 3, 4], expected: 'Error: Side 1 is not a Number' }
+            
+            { sides: [1, 2, 3], expected: ['Error: Not a Triangle',false] },
+            { sides: [4, 'e', 6], expected: ['Error: Side 2 is not a Number',false] },
+            { sides: ['%', 3, 4], expected: ['Error: Side 1 is not a Number',false] },
+            { sides: [3, 3, 3], expected: ['Equilateral',true] },
+            { sides: [5, 5, 8], expected: ['Isosceles',true] },
+            { sides: [5, 4, 3], expected: ['Scalene',true] }
+            
         ];
 
         for(let i=0;i<cases.length;i++){
