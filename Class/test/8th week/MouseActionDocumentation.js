b5f31b573857a880e180e03bfd4edd86b774e60d
Yasmin elkoshy <yasmin.elkoshy@gmail.com>
2025-11-10 19:44:48 +0100

    Updating chromedriver + mouse action

---
+++ Class/test/8th week/MouseActionDocumentation.js
@@ -0,0 +1,52 @@
+const {By, Builder} = require('selenium-webdriver');
+const assert = require("assert");
+require('chromedriver');
+
+describe('Mouse Testsuite', function () {
+  let driver;
+    this.timeout(0);
+  before(async function () {
+    driver = new Builder().forBrowser('chrome').build();
+  });
+
+  after(async () => await driver.quit());
+
+  it('Double-click on an element', async function () {
+    await driver.get('https://www.selenium.dev/selenium/web/mouse_interaction.html');
+    const clickable = driver.findElement(By.id("clickable"));
+    const actions = driver.actions({async: true});
+    await actions.doubleClick(clickable).perform();
+
+    await driver.sleep(500);
+    const status = await driver.findElement(By.id('click-status')).getText();
+    assert.deepStrictEqual(status, `double-clicked`)
+  });
+
+    it('Mouse move into an element', async function () {
+    await driver.get('https://www.selenium.dev/selenium/web/mouse_interaction.html');
+    const hoverable = driver.findElement(By.id("hover"));
+    const actions = driver.actions({async: true});
+    await actions.move({origin: hoverable}).perform();
+
+    const status = await driver.findElement(By.id('move-status')).getText();
+    assert.deepStrictEqual(status, `hovered`)
+  });
+
+    it('Mouse move and right click on an element', async function () {
+    await driver.get('https://www.selenium.dev/selenium/web/mouse_interaction.html');
+    const clickable = driver.findElement(By.id("clickable"));
+    const actions = driver.actions({async: true});
+    await actions.contextClick(clickable).perform();
+
+    await driver.sleep(500);
+    const clicked = await driver.findElement(By.id('click-status')).getText();
+    assert.deepStrictEqual(clicked, `context-clicked`)
+  });
+
+   it('Mouse move and mouseDown on an element', async function () {
+    await driver.get('https://www.selenium.dev/selenium/web/mouse_interaction.html');
+    let clickable = driver.findElement(By.id("clickable"));
+    const actions = driver.actions({async: true});
+    await actions.move({origin: clickable}).press().perform();
+  });
+});
