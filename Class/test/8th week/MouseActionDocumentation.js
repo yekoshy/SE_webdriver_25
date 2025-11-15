97b12256ec1e8c68ad100ae305a792527203dd10
Yasmin elkoshy <yasmin.elkoshy@gmail.com>
2025-11-15 09:57:52 +0100

    Mouse and keyboard interaction v2

--- Class/test/8th week/MouseActionDocumentation.js
+++ Class/test/8th week/MouseActionDocumentation.js
@@ -1,16 +1,20 @@
-const {By, Builder} = require('selenium-webdriver');
+const {By, Builder,Origin} = require('selenium-webdriver');
 const assert = require("assert");
 require('chromedriver');
 
 describe('Mouse Testsuite', function () {
-  let driver;
+    let driver;
     this.timeout(0);
-  before(async function () {
+
+  beforeEach(async function () {
     driver = new Builder().forBrowser('chrome').build();
   });
 
-  //after(async () => await driver.quit());
-after ();
+  afterEach(async function() {
+    await driver.quit()
+
+  });
+
 
 
   it('Double-click on an element', async function () {
@@ -24,7 +28,7 @@ after ();
     assert.deepStrictEqual(status, `double-clicked`)
   });
 
-    it.only('Mouse move into an element', async function () {
+  it('Mouse move into an element', async function () {
     await driver.get('https://www.selenium.dev/selenium/web/mouse_interaction.html');
     const hoverable = driver.findElement(By.id("hover"));
     const actions = driver.actions({async: true});
@@ -51,4 +55,18 @@ after ();
     const actions = driver.actions({async: true});
     await actions.move({origin: clickable}).press().perform();
   });
+
+  it('From current pointer location', async function () {
+    await driver.get('https://selenium.dev/selenium/web/mouse_interaction.html');
+    const actions = driver.actions({async: true});
+    await actions.move({x: 6, y: 3}).perform()
+
+    await actions.move({x: 13, y: 15, origin: Origin.POINTER}).perform()
+
+    let result = await driver.findElement(By.id('absolute-location')).getText();
+    result = result.split(', ');
+    assert.deepStrictEqual(Math.abs(parseInt(result[0]) - 6 - 13) < 2, true)
+    assert.deepStrictEqual(Math.abs(parseInt(result[1]) - 3 - 15) < 2, true)
+  });
+
 });
--- Home/test/8th week/Mouse-n-Keyboard.js
+++ Home/test/8th week/Mouse-n-Keyboard.js
@@ -1,4 +1,4 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
+const {Builder, By, Key, until,Origin, Button} = require('selenium-webdriver');
 require("chromedriver");
 const chrome = require('selenium-webdriver/chrome');
 const { assert } = require('chai');
@@ -14,7 +14,7 @@ class Interaction {
 
     async open() {
         const options = new chrome.Options();
-        options.addArguments("--force-device-scale-factor=0.4");
+        options.addArguments("--force-device-scale-factor=0.5");
         this.driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
         await this.driver.get(this.url);
     }
@@ -24,8 +24,61 @@ class Interaction {
         await n.sendKeys(input);
     }
 
+    async sendTab(){
+        await this.driver.switchTo().activeElement().sendKeys(Key.TAB);
+    }
+
+    async getClass(id){
+        const button = await this.driver.findElement(By.id(id));
+        const classes = await button.getAttribute('class');
+        return classes;
+    }
+
     async click(id){
         await this.driver.findElement(By.id(id)).click()
+        await this.sleep(100);
+    }
+
+    async doubleClick(id){
+        const clickable = await this.driver.findElement(By.id(id));
+        const actions =  this.driver.actions({async: true});
+        await actions.doubleClick(clickable).perform();
+        await this.sleep(100);
+    }
+
+    async mouseDown(id){
+        const clickable = await this.driver.findElement(By.id(id));
+        const actions = this.driver.actions({async: true});
+        await actions.move({origin:clickable}).press(Button.LEFT).perform();
+        await this.sleep(500);
+        
+    }
+
+    async mouseUp(){
+        const actions = this.driver.actions({async: true});
+        await actions.release().perform();
+        await this.sleep(100);
+    }
+
+    async rightClick(id){
+        const clickable = await this.driver.findElement(By.id(id));
+        const actions =  this.driver.actions({async: true});
+        await actions.contextClick(clickable).perform();
+        await this.sleep(100);
+    }
+
+    async moveIn(id){
+        const ele = this.driver.findElement(By.id(id));
+        const actions = this.driver.actions({async: true});
+        await actions.move({origin: ele}).perform();
+        await this.sleep(100);
+        
+    }
+
+    async moveOut(){
+        const actions = this.driver.actions({async: true});
+        await actions.move({x: 50, y: 50, origin: Origin.POINTER}).click().perform()
+        await this.sleep(100);
     }
 
 
@@ -54,6 +107,128 @@ class Interaction {
         await this.driver.quit();
     }
 
+    
+
   
   
 }
+
+
+describe('Testsuit', function () {
+    //remove --no-timeouts
+    this.timeout(0);
+    
+    const objct = new Interaction('https://testpages.eviltester.com/pages/interaction/javascript-events/'); 
+    const expectedTxt = 'Event Triggered'
+    const expectedClass ='styled-click-button-triggered'
+
+    before(async function(){
+        //await objct.open()
+        //await objct.max();
+    })
+
+    beforeEach(async function(){
+        await objct.open()
+        await objct.max();
+    })
+
+    after(async function(){
+        //await objct.close()
+    })
+
+    afterEach(async function(){
+        await objct.close()
+    })
+
+    it('Testing OnBlur',async function(){
+        let id= 'onblur';
+        await objct.click(id)
+        await objct.sendTab();
+        let txt = await objct.getTxt(id+'status');
+        assert.equal(txt,expectedTxt);
+        let classes = await objct.getClass(id);
+        assert.include(classes,expectedClass);
+
+    })
+
+    it('Testing OnClick',async function(){
+        let id='onclick'
+        await objct.click(id)
+        let txt = await objct.getTxt(id+'status');
+        assert.equal(txt,expectedTxt);
+        let classes = await objct.getClass(id);
+        assert.include(classes,expectedClass);
+    })
+
+    it('Testing onContextmenu',async function(){
+        let id= 'oncontextmenu'
+        await objct.rightClick(id)
+        let txt = await objct.getTxt(id+'status');
+        assert.equal(txt,expectedTxt);
+        let classes = await objct.getClass(id);
+        assert.include(classes,expectedClass);
+
+    })
+    it('Testing OnDoubleClick',async function(){
+        let id='ondoubleclick'
+        await objct.doubleClick(id)
+        let txt = await objct.getTxt(id+'status');
+        assert.equal(txt,expectedTxt);
+        let classes = await objct.getClass(id);
+        assert.include(classes,expectedClass);
+    })
+
+    it('Testing OnFocus',async function(){
+        let id='onfocus'
+        await objct.click(id)
+        await objct.sendTab();
+        let txt = await objct.getTxt(id+'status');
+        assert.equal(txt,expectedTxt);
+        let classes = await objct.getClass(id);
+        assert.include(classes,expectedClass);
+    })
+    forEach([['onkeydown',Key.ARROW_DOWN],['onkeyup',Key.ARROW_UP],['onkeypress',Key.SPACE]]).it('Testing %s',async function(name,key){
+        await objct.click(name)
+        await objct.setInput(name,key);
+        let txt = await objct.getTxt(name+'status');
+        assert.equal(txt,expectedTxt);
+        let classes = await objct.getClass(name);
+        assert.include(classes,expectedClass);
+    })
+
+    it('Testing OnMouseOver',async function () {
+        let id='onmouseover'
+        await objct.moveIn(id)
+        let txt = await objct.getTxt(id+'status');
+        assert.equal(txt,expectedTxt);
+        let classes = await objct.getClass(id);
+        assert.include(classes,expectedClass);
+        
+        
+    })
+
+     it('Testing OnMouseLeave',async function () {
+        let id='onmouseleave'
+        await objct.moveIn(id)
+        await objct.moveOut();
+        let txt = await objct.getTxt(id+'status');
+        assert.equal(txt,expectedTxt);
+        let classes = await objct.getClass(id);
+        assert.include(classes,expectedClass);
+        
+        
+    })
+
+    it('Testing OnMouseDown', async function(){
+        let id='onmousedown'
+        await objct.mouseDown(id);
+        let txt = await objct.getTxt(id+'status');
+        assert.equal(txt,expectedTxt);
+        let classes = await objct.getClass(id);
+        assert.include(classes,expectedClass);
+
+        
+    })
+
+    
+})
